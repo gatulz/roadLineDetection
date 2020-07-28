ff0a846f9fcbb54d7183f1624c65b18aef3a1797
gatulz <31404226+gatulz@users.noreply.github.com>
2020-07-28 15:30:48 +0700

    Create source.cpp
    initial file

---
+++ source.cpp
@@ -0,0 +1,14 @@
+// opencvtest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <stdafx.h> 
+#include <iostream>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/photo.hpp>
+#include <opencv2/imgcodecs.hpp>
