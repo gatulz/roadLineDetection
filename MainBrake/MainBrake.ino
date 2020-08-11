5f347a7c3c6cd0244937f776664ce585f8a89cfa
gatulz <gatul111@gmail.com>
2020-08-11 12:14:24 +0700

    Update MainBrake.ino
    merapihkan kode

--- MainBrake/MainBrake.ino
+++ MainBrake/MainBrake.ino
@@ -1,62 +1,47 @@
 
-#define inp1 4 
-#define inp2 5
-#define oneRev 1420
-#define batasCek 300
-#define stallSpeed 10
+#define inp1 4 // digital pin
+#define inp2 5 // digital pin
+#define oneRev 1420 // pulse / rev
 #define torqueThreshold 170
-//encoder
+//encoder pin
 #define enc1A 2 
 #define enc1B 3 
 //PID constant
-#define Kpvs 0.1
-#define Kpv 3.2
-#define Kiv 0.011
-#define Kdv 0
+#define Kps 0.355
+#define Kis 0.0//000145
+#define Kds 1//30
+
 #define TS 11*8+1//33*8 //prescaller timer0 ~7800Hz
 #define max_vs 10
-int calibrate=0;
-int ain=0;
-int kode=0;
-int brake=0;
-int init_ = 0;
-int posMax = -1000;
-bool in1=0;
-bool in2=0;
-
-int brakeON=550*8;
-int brakeREV=400*8;
-int brakeOFF=130*8;
-
-unsigned long lastBrake=0;
-
-int stall =0 ;
+#define brakeOFF 130*8 
 
 //Motor1
 const char fwd1=7; // pin untuk kendali arah motor
 const char rev1=8;
 const char pwm1=6; // pin untuk pwm1
 
+int calibrate=0;
+int ain=0; // torque sensor read (raw)
+int kode=0; // command from serial input
+int brake=0; // brake sign
+int init_ = 0; // variable to initialize brake position
+bool in1=0; // read signal 1 from main arduino
+bool in2=0; // read signal 2 from main arduino
 
-unsigned long lastPID1=0, lastPrint=0;
-
-//PID Pos
-#define Kps 0.355
-#define Kis 0.0//000145
-#define Kds 1//30
 int errPos=0, sumErrPos=0;
 int pwmPos=0, lastErrPos=0;
-
-String inString = "";    // string to hold input
-
 int sp_pos=0, last_sp_pos = 0;
 int sp_vs,  err_vs=0, sumErrVel=0;
-long pos1=0, last_pos1=0;
 int err_v1=0,err_v2=0, v1=0;
 int pwmSpeed=0;
 
+String inString = "";    // string to hold input
 
+unsigned long lastBrake=0;
+unsigned long lastPID1=0, lastPrint=0;
+long pos1=0, last_pos1=0;
 
+//function to read position and direction of hall sensor (encoder)
 void ISR_INT0(){
   int pinA,pinB;
   pinA=digitalRead(enc1A);
@@ -86,7 +71,7 @@ void ISR_INT1(){
   }
 }
 
-//use to generate PWM to motor driver
+//function to generate PWM to motor driver
 void motor_power(int pwm, int fwd, int rev, int power){
   int pwm_value;
   pwm_value=abs(power);
@@ -107,7 +92,7 @@ void motor_power(int pwm, int fwd, int rev, int power){
   }
 }
 
-//set a maximum and minimum threshold of a variable
+//function to set a maximum and minimum threshold of a variable
 void threshold(int *pwmval, int maxValue){
   if (*pwmval > maxValue){
     *pwmval = maxValue;
@@ -116,7 +101,7 @@ void threshold(int *pwmval, int maxValue){
   } 
 }
 
-// funtion to give 
+// funtion to give command to brake motor through serial monitor
 void strToInt(int *setpoint){
   while (Serial.available() > 0) {
     int inChar = Serial.read();
@@ -143,8 +128,10 @@ void setup() {
   pinMode(enc1B,INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(enc1A), ISR_INT0, CHANGE);
   attachInterrupt(digitalPinToInterrupt(enc1B), ISR_INT1, CHANGE);
-  
-  TCCR0B = TCCR0B & B11111000 | B00000010;    // set timer 0 divisor to     8 for PWM frequency of  7812.50 Hz
+
+  // set timer 0 divisor to  8 for PWM frequency of  7812.50 Hz
+  // timer set to 8kHz to reduce sound of motor while moving
+  TCCR0B = TCCR0B & B11111000 | B00000010;    
   //motor1
   pinMode(rev1, OUTPUT);   
   pinMode(fwd1, OUTPUT);
@@ -154,25 +141,23 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //last_sp_pos = sp_pos;
 
-  //strToInt(&kode);//sp_pos);
+  //strToInt(&kode);//sp_pos); // use to test command using serial monitor
   readInput();
 
   if (millis() - lastPID1>=(TS)){
     samplingEnc();
-    if ((abs(sp_pos - pos1)>40) )
+    if ((abs(sp_pos - pos1)>40) ) // use for move the motor to initial position
       pidPosisi();//KecPos();
     lastPID1 = millis();
   }
-  if (millis() - lastPrint>=561){  
+  if (millis() - lastPrint>=561){  //print data for debugging
     printData();
     lastPrint=millis();
   }
 
   if (!init_){
-    init_brake();
+    init_brake(); // initialize brake position
   } else {
     remON();
   }
@@ -191,6 +176,7 @@ void printData(){
   
 }
 
+// function ro read input from main arduino
 void readInput(){
   ain=analogRead(A0); //current of brake motor I (mA) = (ain/1023)*(40/13.3)
   in1 = digitalRead(inp1); //brake signal from main controller
@@ -207,12 +193,13 @@ void readInput(){
   }
 }
 
-
+// sampling of motor speed
 void samplingEnc(){
   v1 = pos1 - last_pos1;
   last_pos1 = pos1;
 }
 
+// Position PID Algoritm
 void pidPosisi(){
   
   errPos = sp_pos - pos1;
@@ -226,8 +213,8 @@ void pidPosisi(){
   lastErrPos = errPos;
 }
 
+// function to initialize brake position 
 void init_brake(){
-  
   if (millis() <17000){ //2 detik (2*8000
     if (calibrate==0){
       motor_power(pwm1, rev1, fwd1, 230);
@@ -236,14 +223,12 @@ void init_brake(){
         //sp_pos = pos1-440;
         lastBrake=millis();
       }
-      
     } else {
       if (millis() - lastBrake<1600)
         motor_power(pwm1, rev1, fwd1, -110);
       else 
         motor_power(pwm1, rev1, fwd1, 0);
     }
-    
   } else {
     init_=1;
     motor_power(pwm1, rev1, fwd1, 0);
@@ -251,8 +236,9 @@ void init_brake(){
   }
 }
 
+// torque control
 void remON(){
-  if (brake==1){
+  if (brake==1){ // brake is on
     if (ain<torqueThreshold){      
       lastBrake=millis();
       motor_power(pwm1, rev1, fwd1, 220);
@@ -261,9 +247,9 @@ void remON(){
     } else {
         motor_power(pwm1, rev1, fwd1, pwmPos);
     }
-  } else if ((brake==2) || (brake==3)){
+  } else if ((brake==2) || (brake==3)){ // brake off -  move to initial position
         motor_power(pwm1, rev1, fwd1, pwmPos);
-  } else if (brake==0){
+  } else if (brake==0){ // brake system is inactive
       motor_power(pwm1, rev1, fwd1,0);
   }
 }
